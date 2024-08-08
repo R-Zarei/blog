6dca47424bcdace6b84cd2daeb7726c7e0b305d7
reza <rzareei672@gmail.com>
2024-08-08 12:29:15 +0330

    Implementation of login mechanism in the login view of account app.

--- account_app/views.py
+++ account_app/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
-def login(request):
-    return render(request, template_name='account_app/login.html', context={})
+def login_view(request):
+    if not request.user.is_authenticated:   # can use .is_anonymous instead .is_authenticated.
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+        return render(request, template_name='account_app/login.html', context={})
+
+    else:
+        return redirect('/')

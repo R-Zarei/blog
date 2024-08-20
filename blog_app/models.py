85217574a92a9437c09a01b3c64207ff24dfc470
reza <rzareei672@gmail.com>
2024-08-20 12:25:09 +0330

    add pub_date in blog_app/models.Post and add unique_for_date in title of this model.

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique_for_date='pub_date')
     body = models.TextField()
     image = models.ImageField(upload_to='img/post')
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
+    pub_date = models.DateField(default=timezone.now())
 
     def __str__(self):
         return self.title

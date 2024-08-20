392837cc5d567a886bca682563afcb7d1181597f
reza <rzareei672@gmail.com>
2024-08-20 10:53:14 +0330

    create relationship many of posts to one user in post model of blog_app and send posts to index.html of home_app with home_app/views.py and change to index.html.

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
     body = models.TextField()
     image = models.ImageField(upload_to='img/post')
     created = models.DateField(auto_now_add=True)
--- home_app/views.py
+++ home_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from blog_app.models import Post
 
 
 def home(request):
-    return render(request, template_name='home_app/index.html', context={})
+    posts = Post.objects.all()
+    return render(request, template_name='home_app/index.html', context={'posts': posts})

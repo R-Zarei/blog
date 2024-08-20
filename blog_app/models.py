4cdf2ef131ec560a0c97553c9c5f9ad58f92816c
reza <rzareei672@gmail.com>
2024-08-20 18:06:38 +0330

    create Category model in blog_app and add category attribute to Post model and connect it to Category model with manytomany rel and making the categories dynamic in index.html

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,9 +3,18 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, unique_for_date='pub_date')
+    category = models.ManyToManyField(Category)
     body = models.TextField()
     image = models.ImageField(upload_to='img/post')
     created = models.DateField(auto_now_add=True)

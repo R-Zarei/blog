ab609d964b85058017fa82d8d16235de0c7dd307
reza <rzareei672@gmail.com>
2024-09-02 14:37:08 +0330

    add published attribute and personalization model.manager with ModelManger class in blog_app\models.py.

--- blog_app/models.py
+++ blog_app/models.py
@@ -11,15 +11,26 @@ class Category(models.Model):
         return self.title
 
 
+class ModelManager(models.Manager):
+    def counter(self):
+        return len(self.all())
+
+    def published(self):
+        return self.first(published=True)
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, unique_for_date='pub_date')
     category = models.ManyToManyField(Category)
     body = models.TextField()
     image = models.ImageField(upload_to='img/post')
+    published = models.BooleanField(default=True)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
     pub_date = models.DateField(default=timezone.now())
+
+    objects = ModelManager()
 
     def __str__(self):
         return self.title

d6b4c97595e706f63e85d479e464f85e1d8ffd2a
reza <rzareei672@gmail.com>
2024-09-04 00:08:07 +0330

    make post_details view in blog_app and it url and add post_details.html in blog_app.

--- blog/urls.py
+++ blog/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_app.urls')),
     path('', include('account_app.urls')),
+    path('', include('blog_app.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_details(request, pk):
+    context = Post.objects.get(pk=pk)
+    return render(request, 'blog_app/post_details.html', {'post': context})

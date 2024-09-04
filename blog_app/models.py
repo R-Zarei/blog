42fbb871e8e2b0f7751b9520dcabad53b2f1f5e1
reza <rzareei672@gmail.com>
2024-09-04 18:37:49 +0330

    use inheritance in index and post_details templates and create templates\base.html and creat get_absolute_url metod in Post model.

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -31,6 +32,9 @@ class Post(models.Model):
     pub_date = models.DateField(default=timezone.now())
 
     objects = ModelManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_details', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title

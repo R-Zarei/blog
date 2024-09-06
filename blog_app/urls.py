525fc487a3e76264ad4e4dc0113843075669ca51
reza <rzareei672@gmail.com>
2024-09-06 17:42:02 +0330

    Implementation post_list page (add template, create view in blog_app, add url).

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('posts/<int:pk>/', views.post_details, name='post_details'),
+    path('posts', views.post_list, name='posts'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,5 +3,14 @@ from .models import Post
 
 
 def post_details(request, pk):
-    context = Post.objects.get(pk=pk)
-    return render(request, 'blog_app/post_details.html', {'post': context})
+    post = Post.objects.get(pk=pk)
+    recent_posts = Post.objects.all().order_by('-created')[:3]
+    return render(request, 'blog_app/post_details.html', {'post': post, 'recent_posts': recent_posts})
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    recent_posts = Post.objects.all().order_by('-created')[:3]
+    return render(request, template_name='blog_app/posts_list.html',
+                  context={'posts': posts, 'recent_posts': recent_posts})
+
